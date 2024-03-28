2eb83270ee82e86fd1ce02136e7eb30d7ac422dd
MartaDomanska <martadomanska@MacBook-Pro-MartaDomanska.local>
2024-03-28 14:25:40 +0100

    resolved challenge

--- src/warm-up/warm-up.ts
+++ src/warm-up/warm-up.ts
@@ -9,9 +9,15 @@
 * Hint: https://www.typescriptlang.org/docs/handbook/basic-types.html
 */
 
-const age: string = 80;
-const firstName: number = 'Przemek';
-const todos: Map = [
+const age: number = 80;
+const firstName: string = 'Przemek';
+
+interface Todo {
+    todo: string;
+    priority?: number;
+}
+
+const todos: Todo[] = [
     { todo: 'Learn TypeScript' },
     { todo: 'Subscribe Przeprogramowani', priority: 1},
     { todo: 'Sign up for newsletter', priority: 1}
