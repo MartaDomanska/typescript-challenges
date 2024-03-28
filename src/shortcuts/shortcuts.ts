ca5ed97fb157e2af2ae8372a546108aa8ce17945
MartaDomanska <martadomanska@MacBook-Pro-MartaDomanska.local>
2024-03-28 08:42:20 +0100

    resolved challenge

--- src/shortcuts/shortcuts.ts
+++ src/shortcuts/shortcuts.ts
@@ -1,36 +1,39 @@
 /*
-* Przeprogramowani.ts - https://przeprogramowani.pl/typescript/
-*
-* Shortcuts
-* ------------------
-*
-* Goal: Make sure that everything works fine - it's that simple!
-* 
-* Hint: https://www.typescriptlang.org/docs/handbook/classes.html
-*/
+ * Przeprogramowani.ts - https://przeprogramowani.pl/typescript/
+ *
+ * Shortcuts
+ * ------------------
+ *
+ * Goal: Make sure that everything works fine - it's that simple!
+ *
+ * Hint: https://www.typescriptlang.org/docs/handbook/classes.html
+ */
 
 class Bag {
-    constructor(
-        items: string[]
-    ) { }
+  items: string[] = [];
 
-    lookInside() {
-        return this.items;
-    }
+  constructor(items: string[]) {
+    this.items = items;
+  }
+
+  lookInside() {
+    return this.items;
+  }
 }
 
 class BrandedBag extends Bag {
-    constructor(
-        logo: string,
-        brand: string,
-        items: string[]
-    ) {
-        super(items);
-    }
+  logo: string;
+  brand: string;
 
-    checkBrand() {
-        return `Brand - ${this.brand}`;
-    }
+  constructor(logo: string, brand: string, items: string[]) {
+    super(items);
+    this.logo = logo;
+    this.brand = brand;
+  }
+
+  checkBrand() {
+    return `Brand - ${this.brand}`;
+  }
 }
 
 const devBag = new BrandedBag('<P/>', 'Przeprogramowani.pl', ['keys', 'sunglassess', 'books']);
